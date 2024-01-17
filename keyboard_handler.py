13ab3f8840da3e4eace1b00e9c6ca6299897ba42
SohamNandiPDX <snandi@pdx.edu>
2024-01-16 22:48:06 -0800

    Defined KeyboardEvent structure

---
+++ keyboard_handler.py
@@ -0,0 +1,22 @@
+import ctypes
+import time
+
+# Define key constants
+KEY_W = 0x11
+KEY_A = 0x1E
+KEY_S = 0x1F
+KEY_D = 0x20
+KEY_SPACE = 0x39
+
+PTR_UL = ctypes.POINTER(ctypes.c_ulong)
+
+# Structuring to represent a Keyboard Event
+class KeyboardEvent(ctypes.Structure):
+    _fields_ = [
+        ("virtual_key", ctypes.c_ushort),
+        ("scan_code", ctypes.c_ushort),
+        ("flags", ctypes.c_ulong),
+        ("timestamp", ctypes.c_ulong),
+        ("extra_info", PTR_UL)
+    ]
+
