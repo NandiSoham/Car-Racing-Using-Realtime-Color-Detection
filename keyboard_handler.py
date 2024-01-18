11da6509e4cb80a4d7230c10792d1109ffb7d951
SohamNandiPDX <snandi@pdx.edu>
2024-01-17 22:35:21 -0800

    Define MouseEvent and HardwareEvent structure

--- keyboard_handler.py
+++ keyboard_handler.py
@@ -20,3 +20,21 @@ class KeyboardEvent(ctypes.Structure):
         ("extra_info", PTR_UL)
     ]
 
+# Define HardwareEvent structure for hardware input
+class HardwareEvent(ctypes.Structure):
+    _fields_ = [
+        ("message", ctypes.c_ulong),
+        ("param_low", ctypes.c_short),
+        ("param_high", ctypes.c_ushort)
+    ]
+
+# Define MouseEvent structure for mouse input
+class MouseEvent(ctypes.Structure):
+    _fields_ = [
+        ("x_movement", ctypes.c_long),
+        ("y_movement", ctypes.c_long),
+        ("mouse_data", ctypes.c_ulong),
+        ("flags", ctypes.c_ulong),
+        ("timestamp", ctypes.c_ulong),
+        ("extra_info", PTR_UL)
+    ]
